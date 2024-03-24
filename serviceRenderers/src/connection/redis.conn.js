5f67113f0d218bbd5d1a69c33ef1b49243863779
feyishola <feyisholaologunebi@gmail.com>
2024-03-24 12:41:15 +0100

    added try catch to redis connection

--- serviceRenderers/src/connection/redis.conn.js
+++ serviceRenderers/src/connection/redis.conn.js
@@ -1,28 +1,32 @@
 const redis = require("redis");
 
 module.exports = async () => {
-  const redisPort = process.env.REDIS_PORT || 6379;
-  const client = redis.createClient({
-    url: "rediss://oregon-redis.render.com:6379",
-    username: "red-ckoj2c0ujous73eg1gv0",
-    password: "CUq8rhCoCknwxxuMHZiOkod6moHl85TS",
-  });
+  try {
+    const redisPort = process.env.REDIS_PORT || 6379;
+    const client = redis.createClient({
+      url: "rediss://oregon-redis.render.com:6379",
+      username: "red-ckoj2c0ujous73eg1gv0",
+      password: "CUq8rhCoCknwxxuMHZiOkod6moHl85TS",
+    });
 
-  // const client = redis.createClient({
-  //   url: "rediss://127.0.0.1:6379",
-  // });
+    // const client = redis.createClient({
+    //   url: "rediss://127.0.0.1:6379",
+    // });
 
-  await client.connect();
+    await client.connect();
 
-  // Test the connection
+    // Test the connection
 
-  client.on("ready", () => {
-    console.log("connected to redis successfully!");
-  });
+    client.on("ready", () => {
+      console.log("connected to redis successfully!");
+    });
 
-  client.on("error", (err) => {
-    console.log("Redis connection error", err);
-  });
+    client.on("error", (err) => {
+      console.log("Redis connection error", err);
+    });
 
-  return client;
+    return client;
+  } catch (error) {
+    console.log("Redis connection error", error.message);
+  }
 };
